42f6c6cb8ad171d0d81ccaf75e8e608f4bb46ed2
manjari5506 <97456735+manjari5506@users.noreply.github.com>
2022-03-03 13:51:06 +0530

    all files commit
    simple calculator files

---
+++ divide.js
@@ -0,0 +1,6 @@
+function divide(x,y) {
+    var div = x / y;
+    return div;
+  }
+
+  module.exports = divide ;
---
+++ subtract.js
@@ -0,0 +1,6 @@
+function subtract(x,y) {
+    var diff = x - y;
+    return diff;
+  }
+
+  module.exports = subtract ;
